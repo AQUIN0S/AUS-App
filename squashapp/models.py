2d0ecb85bca72c0c68d330f0ab5fee1c2601b826
Daniel Schimanski <danny.manski135@gmail.com>
2019-08-07 14:47:47 +1200

    Restarting application
    It would be more beneficial to begin this project again and follow through with the proper steps, rather than trying to take shortcuts after only one tutorial

--- squashapp/models.py
+++
@@ -1,8 +0,0 @@
-from squashapp import db, app
-
-class Player(db.Model):
-    id = db.Column(Db.Integer, primary_key=True)
-    first_name = db.Column(db.String(30), nulable=False)
-    last_name = db.Column(db.String(30), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    
